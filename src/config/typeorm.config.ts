024fe9517dc12e3c5eaf0c7c1b69d6ae9a05211f
Kamil Toś <kamil.tos@innvia.com>
2020-03-25 21:40:45 +0100

    fix

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,17 +3,17 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig: TypeOrmModuleOptions = process.env.DATABASE_URL
   ? {
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
+      url: process.env.DATABASE_URL,
       database: 'taskmanagement',
       entities: [__dirname + '/../**/*.entity.{js,ts}'],
       synchronize: true,
     }
   : {
       type: 'postgres',
-      url: process.env.DATABASE_URL,
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
       database: 'taskmanagement',
       entities: [__dirname + '/../**/*.entity.{js,ts}'],
       synchronize: true,
